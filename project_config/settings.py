5657ad7c20a29861e1a8e5724329a2daa67c6aae
cnrblake <cnrblakefinance2@zohomail.eu>
2026-03-17 10:24:58 +0000

    CReated a table view to display open at risk assets using bootstrap

--- project_config/settings.py
+++ project_config/settings.py
@@ -119,6 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# Django Authentication Redirect Rules
-LOGIN_REDIRECT_URL = '/admin/'  # We will change this to the Dashboard once we build it!
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
--- vams_application/views.py
+++ vams_application/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Finding
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login')
+def dashboard(request):
+    all_findings = Finding.objects.select_related('assetid', 'vuldefid').all()
+
+    context = {
+        'findings': all_findings
+    }
+    return render(request, 'vams_application/dashboard.html', context)
