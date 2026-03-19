bc33a2c5bf574f4193249ece11c9bbcf23a88ee8
cnrblake <cnrblakefinance2@zohomail.eu>
2026-03-19 13:47:34 +0000

    implemented testing

--- vams_application/tests.py
+++ vams_application/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class SystemSecurityTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            "testadmin", "test@example.com", "testpass123"
+        )
+
+    def test_admin_panel(self):
+        """Test that a logged-in admin can access the backend."""
+        self.client.login(username="testadmin", password="testpass123")
+        response = self.client.get("/admin/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_unauthorized_access(self):
+        self.client.logout()
+        response = self.client.get("/admin/")
+        self.assertNotEqual(response.status_code, 200)
